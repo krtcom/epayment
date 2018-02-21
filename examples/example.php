7818725297081e9ea7c76694dac9a5cd0905084e
Michal Krnáč <krtcom@gmail.com>
2018-02-21 23:56:31 +0100

    rename test to examples

---
+++ examples/example.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use EPayments\TB_CardPay;
+
+define('EPAYMENT_TB_CARDPAY_MID', "9999");
+define('EPAYMENT_TB_CARDPAY_SECRET', "31323334353637383930313233343536373839303132333435363738393031323132333435363738393031323334353637383930313233343536373839303132");
+
+$payment = new TB_CardPay();
+
+echo $payment->request(6.5, '123456', 'https://www.example.com', 'Michal Krnáč');
